81eee4191c982bf7f32a8ad50ffbafdc292e4e62
Abhishek-K-U <abhishekkugowda@gmail.com>
2026-03-29 00:29:21 +0530

    finishing basic

--- UDEMYFULLSTACK/emongoose/index.js
+++ UDEMYFULLSTACK/emongoose/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path=require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost:27017/exmongodb')
 .then(()=>{
     console.log("MONGO CONNECTION OPEN!!!");
@@ -15,12 +16,53 @@ mongoose.connect('mongodb://localhost:27017/exmongodb')
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+const categories=['fruit','vegetable','dairy','meat','other'];
 app.get('/products',async(req,res)=>{
-    const products = await Product.find({})
-    console.log(products);  
-    res.send('All Products');
+    const { category } = req.query;
+    if(category){
+        const products = await Product.find({category})
+        res.render('products/index',{products,category});
+    }else{
+         const products = await Product.find({})
+        res.render('products/index',{products,category:'All'});
+    }
 });
+app.get('/products/new', (req,res)=>{
+    res.render('products/new' ,{categories});
+})
+
+app.post('/products',async(req,res)=>{
+    const newProduct = new Product(req.body);
+    await  newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id',async(req,res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show',{product});
+})
+app.get('/products/:id/edit',async(req,res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit',{product,categories});
+})
+
+app.put('/products/:id',async(req,res)=>{
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true});
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id',async(req,res)=>{
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
