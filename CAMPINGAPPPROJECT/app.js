91b3a472d254d130871067b0df7cd37ba86a8f49
Abhishek-K-U <abhishekkugowda@gmail.com>
2026-04-01 18:08:30 +0530

    3rd day

--- CAMPINGAPPPROJECT/app.js
+++ CAMPINGAPPPROJECT/app.js
@@ -16,7 +16,7 @@ db.once("open",()=>{
     console.log("Database connected");
 });
 const app=express();
-
+app.use(express.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -25,10 +25,22 @@ app.set('views',path.join(__dirname,'views'));
 app.get("/",(req,res)=>{
     res.render('home');
 });
-app.get('/makecampground',async (req,res)=>{
-    const camp=new Campground({title:"HomeYard",price:100,description:"Beautiful Campground",location:"India"});
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds",async(req,res)=>{
+    const campgrounds=await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+});
+app.get("/campgrounds/new",(req,res)=>{
+    res.render('campgrounds/new');
+});
+app.post("/campgrounds",async(req,res)=>{
+    const campground=new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
 });
 
 
