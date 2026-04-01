75caad92dced6207313d56fae23898e232fb151e
Abhishek-K-U <abhishekkugowda@gmail.com>
2026-04-01 18:08:30 +0530

    3rd day

--- CAMPINGAPPPROJECT/seeds/index.js
+++ CAMPINGAPPPROJECT/seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
 const Campground=require('../models/campground');
+const cities=require('./cities');
+const {descriptors,places}=require('./seedHelpers');
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 .then(()=>{
     console.log("Mongo Connection Open");
@@ -14,11 +16,19 @@ db.once("open",()=>{
     console.log("Database connected");
 });
 
-
+const sample=array=>array[Math.floor(Math.random()*array.length)];
 const seedDB=async()=>{
     await Campground.deleteMany({});    
-    const camp=new Campground({title:"Home",price:100,description:"Beautiful Campground",location:"KArnataka"});
-    await camp.save();
+    for(let i=0;i<50;i++){
+        const random1000=Math.floor(Math.random()*1000);
+        const camp=new Campground({
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title:`${sample(descriptors)} ${sample(places)}`,
+        });
+        await camp.save();
+    }       
 }
 
-seedDB();
+seedDB().then(()=>{
+    mongoose.connection.close();
+});
