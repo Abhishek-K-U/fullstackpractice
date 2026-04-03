14bb8c76185abb14db72b17dc1c61090532ca935
Abhishek-K-U <abhishekkugowda@gmail.com>
2026-04-03 20:42:55 +0530

    4th day

--- CAMPINGAPPPROJECT/app.js
+++ CAMPINGAPPPROJECT/app.js
@@ -2,6 +2,8 @@ const express=require("express");
 const mongoose=require("mongoose");
 const path=require("path");
 const Campground=require('./models/campground');
+const methodOverride=require("method-override");
+const  morgan=require("morgan");
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 .then(()=>{
     console.log("Mongo Connection Open");
@@ -16,8 +18,9 @@ db.once("open",()=>{
     console.log("Database connected");
 });
 const app=express();
+app.use(morgan('tiny'));
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride('_method'));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
@@ -42,7 +45,20 @@ app.get('/campgrounds/:id',async(req,res)=>{
     const campground=await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground});
 });
-
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+});
+app.put('/campgrounds/:id',async(req,res)=>{
+    const {id}=req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+app.delete('/campgrounds/:id',async(req,res)=>{
+    const {id}=req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
 
 
 
