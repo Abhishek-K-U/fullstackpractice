d75a1e51000b2b7daf97ff7f17577ff1fc06e530
Abhishek-K-U <abhishekkugowda@gmail.com>
2026-04-25 23:27:44 +0530

    Day 5

--- CAMPINGAPPPROJECT/app.js
+++ CAMPINGAPPPROJECT/app.js
@@ -4,6 +4,7 @@ const path=require("path");
 const Campground=require('./models/campground');
 const methodOverride=require("method-override");
 const  morgan=require("morgan");
+const ejsMate=require("ejs-mate");
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 .then(()=>{
     console.log("Mongo Connection Open");
@@ -18,13 +19,14 @@ db.once("open",()=>{
     console.log("Database connected");
 });
 const app=express();
+app.engine('ejs',ejsMate);
 app.use(morgan('tiny'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.use((req,res,next)=>{
+const verPass= ((req,res,next)=>{
     const {password} = req.query;
     if(password==='chicken'){
        return next();
@@ -42,30 +44,33 @@ app.get("/campgrounds",async(req,res)=>{
 app.get("/campgrounds/new",(req,res)=>{
     res.render('campgrounds/new');
 });
-// app.post("/campgrounds",async(req,res)=>{
-//     const campground=new Campground(req.body.campground);
-//     await campground.save();
-//     res.redirect(`/campgrounds/${campground._id}`);
-// });
+app.get('/secret',verPass ,(req,res)=>{
+    res.send("The secret is 42");
+});
+app.post("/campgrounds",async(req,res)=>{
+    const campground=new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
-// app.get('/campgrounds/:id',async(req,res)=>{
-//     const campground=await Campground.findById(req.params.id);
-//     res.render('campgrounds/show',{campground});
-// });
-// app.get('/campgrounds/:id/edit',async(req,res)=>{
-//     const campground=await Campground.findById(req.params.id);
-//     res.render('campgrounds/edit',{campground});
-// });
-// app.put('/campgrounds/:id',async(req,res)=>{
-//     const {id}=req.params;
-//     const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground});
-//     res.redirect(`/campgrounds/${campground._id}`);
-// });
-// app.delete('/campgrounds/:id',async(req,res)=>{
-//     const {id}=req.params;
-//     await Campground.findByIdAndDelete(id);
-//     res.redirect('/campgrounds');
-// });
+app.get('/campgrounds/:id',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+});
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+});
+app.put('/campgrounds/:id',async(req,res)=>{
+    const {id}=req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+app.delete('/campgrounds/:id',async(req,res)=>{
+    const {id}=req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
 
 
 
